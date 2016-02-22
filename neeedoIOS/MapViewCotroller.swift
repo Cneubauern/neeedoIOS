b0a331b194fe5e7d640ab726f2cd26dd0a351d40
cneubauern <cneubauern@googlemail.com>
2016-02-22 12:21:23 +0100

    added Maps and UserLocation

---
+++ neeedoIOS/MapViewCotroller.swift
@@ -0,0 +1,89 @@
+//
+//  MapViewCotroller.swift
+//  neeedoIOS
+//
+//  Created by Christian Neubauer on 22.02.16.
+//  Copyright © 2016 cneubauern. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+
+        uilpgr.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func action(gestureRecognizer: UIGestureRecognizer){
+        
+        print("gesture recognized")
+        
+        let touchPoint = gestureRecognizer.locationInView(self.map)
+        
+        let newCoordinate:CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        placeAnnotation(newCoordinate, title: "You Touched Here", subtitle: "Why?")
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation:CLLocation = locations[0]
+        
+        let latitude:CLLocationDegrees = userLocation.coordinate.latitude
+        let longitude:CLLocationDegrees = userLocation.coordinate.longitude
+        let latDelta:CLLocationDegrees = 0.05
+        let lonDelta:CLLocationDegrees = 0.05
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        self.map.setRegion(region, animated: true)
+    
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func placeAnnotation(location:CLLocationCoordinate2D, title: String, subtitle:String ){
+        
+        let annotation = MKPointAnnotation()
+
+        annotation.coordinate = location
+        
+        annotation.title = title
+        
+        annotation.subtitle = subtitle
+        
+        map.addAnnotation(annotation)
+        
+    }
+    
+}
